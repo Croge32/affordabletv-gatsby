404324f841a9b33a128a923f7794e911f4ef712a
Cody Rogers <croge32@gmail.com>
2018-01-30 14:19:17 -0600

    Add glamorous and absolute file path support
    This commit adds glamor and glamorous plugins for styled component use, and
    also adds gatsby's plugin to allow absolute importing instead of relative
    importing

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,31 +1,35 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react"
+import PropTypes from "prop-types"
+import Link from "gatsby-link"
+import Helmet from "react-helmet"
 
-import Header from '../components/Header'
-import './index.css'
+import "./index.css"
+import glamorous from "glamorous"
+
+const Header = () => (
+  <HeaderContainer>
+    <HeaderTextContainer>
+      <HeaderText>
+        <HeaderLink to="/">Gatsby</HeaderLink>
+      </HeaderText>
+    </HeaderTextContainer>
+  </HeaderContainer>
+)
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <Container>
     <Helmet
       title="Gatsby Default Starter"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" },
       ]}
     />
+
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
+
+    <TemplateContainer>{children()}</TemplateContainer>
+  </Container>
 )
 
 TemplateWrapper.propTypes = {
@@ -33,3 +37,32 @@ TemplateWrapper.propTypes = {
 }
 
 export default TemplateWrapper
+
+const Container = glamorous.div({})
+
+const HeaderContainer = glamorous.div({
+  background: "rebeccapurple",
+  marginBottom: "1.45rem",
+})
+
+const HeaderTextContainer = glamorous.div({
+  margin: "0 auto",
+  maxWidth: 960,
+  padding: "1.45rem 1.0875rem",
+})
+
+const HeaderText = glamorous.h1({
+  margin: 0,
+})
+
+const HeaderLink = glamorous(Link)({
+  color: "white",
+  textDecoration: "none",
+})
+
+const TemplateContainer = glamorous.div({
+  margin: "0 auto",
+  maxWidth: 960,
+  padding: "0px 1.0875rem 1.45rem",
+  paddingTop: 0,
+})
